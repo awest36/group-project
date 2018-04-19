f21cb85cffff93b1e26ece8e112267b0cacc6031
Alexandria West <32424732+awest36@users.noreply.github.com>
2018-04-19 14:59:27 -0500

    project
    update to full aplication, login, login out, sign in, profile

---
+++ models/user.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    Bio: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true
+    }
+});
+// authenticate input against database documents
+UserSchema.statics.authenticate = function(email, password, callback) {
+  User.findOne({ email: email })
+      .exec(function (error, user) {
+        if (error) {
+          return callback(error);
+        } else if ( !user ) {
+          var err = new Error('User not found.');
+          err.status = 401;
+          return callback(err);
+        }
+        bcrypt.compare(password, user.password , function(error, result) {
+          if (result === true) {
+            return callback(null, user);
+          } else {
+            return callback();
+          }
+        })
+      });
+}
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
